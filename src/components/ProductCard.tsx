96d787b9397575b07863fb7c02b301cd7a92c267
Enzo Gimena <enzogimena.shawn@gmail.com>
2025-12-10 18:14:49 +0800

    second comit

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,6 +18,7 @@ const ProductCard = ({ id, name, price, image, description }: ProductCardProps) 
           <img
             src={image}
             alt={name}
+            loading="lazy"
             className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
           />
         </div>
